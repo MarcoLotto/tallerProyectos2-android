4da59e9a0dd6f06d5dd587f4a92660c02a8a1f68
MarcoLotto <marcol91@gmail.com>
2015-04-24 18:38:38 -0300

    Me quedo una cosa mal del ultimo merge

--- app/src/main/java/com/example/marco/fiubados/TabScreens/FriendsTabScreen.java
+++ app/src/main/java/com/example/marco/fiubados/TabScreens/FriendsTabScreen.java
@@ -12,6 +12,7 @@ import com.example.marco.fiubados.httpAsyncTasks.SearchFriendsHttpAsyncTask;
 import com.example.marco.fiubados.model.User;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -31,7 +32,7 @@ public class FriendsTabScreen implements TabScreen{
     public FriendsTabScreen(TabbedActivity tabOwnerActivity, ListView friendsListView){
         this.tabOwnerActivity = tabOwnerActivity;
         this.friendsListView = friendsListView;
-        this.users = new ArrayList<>();
+        this.users = new ArrayList<User>();
         this.configureComponents();
     }
 
@@ -59,69 +60,44 @@ public class FriendsTabScreen implements TabScreen{
     public void onFocus() {
         this.users.clear();
 
-<<<<<<< HEAD
-=======
-        // Vamos a hacer el pedido de busqueda de usuarios
-        SearchFriendsHttpAsyncTask searchUsersHttpService = new SearchFriendsHttpAsyncTask(this.tabOwnerActivity, this,
-                SEARCH_USERS_SERVICE_ID, "TODO");
-        searchUsersHttpService.execute(SEARCH_USERS_ENDPOINT_URL);
-
->>>>>>> c639f520a2cc16539218dbb5335f10a9123e1cb7
         // Vamos a hacer el pedido de amigos al web service
         GetFriendsHttpAsyncTask friendsHttpService = new GetFriendsHttpAsyncTask(this.tabOwnerActivity, this,
-                SEARCH_FRIENDS_SERVICE_ID, "TODO");
-        friendsHttpService.execute(FRIENDS_SEARCH_ENDPOINT_URL);
+                this.SEARCH_FRIENDS_SERVICE_ID, "TODO");
+        friendsHttpService.execute(this.FRIENDS_SEARCH_ENDPOINT_URL);
     }
 
     @Override
     public void onServiceCallback(List responseElements, int serviceId) {
-<<<<<<< HEAD
         if(serviceId == this.SEARCH_FRIENDS_SERVICE_ID){
             this.users = responseElements;
             Iterator<User> it = this.users.iterator();
             while(it.hasNext()){
                 User user = it.next();
-=======
-        if(serviceId == this.SEARCH_USERS_SERVICE_ID){
-            this.fillUserLists(responseElements);
-        }
-        if(serviceId == SEARCH_FRIENDS_SERVICE_ID){
-            for (User user : this.users) {
->>>>>>> c639f520a2cc16539218dbb5335f10a9123e1cb7
                 if(responseElements.contains(user)){
                     // Mi usuario y este usuario son amigos
                     user.setFriendshipStatus(User.FRIENDSHIP_STATUS_FRIEND);
                 }
             }
             this.addUsersToUserUIList(this.users, this.friendsListView);
-
         }
     }
 
     private void fillUserLists(List responseElements) {
-        for (User user : (List<User>) responseElements) {
-            this.users.add(user);
+        Iterator<User> it = responseElements.iterator();
+        while(it.hasNext()){
+            this.users.add(it.next());
         }
     }
 
     private void addUsersToUserUIList(List<User> usersList, ListView usersListView) {
-        List<String> finalListViewLines = new ArrayList<>();
-
-        for (User user : usersList) {
+        List<String> finalListViewLines = new ArrayList<String>();
+        Iterator<User> it = usersList.iterator();
+        while(it.hasNext()){
             // Agregamos a la lista de amigos a todos los usuarios
-<<<<<<< HEAD
             User user = it.next();
             finalListViewLines.add(user.getName());
-=======
-            String finalString = user.getName();
-            if(user.getFriendshipStatus().equals(User.FRIENDSHIP_STATUS_FRIEND)){
-                finalString += " - Amigo";
-            }
-            finalListViewLines.add(finalString);
->>>>>>> c639f520a2cc16539218dbb5335f10a9123e1cb7
         }
-
-        ArrayAdapter adapter = new ArrayAdapter<>(this.tabOwnerActivity, android.R.layout.simple_list_item_1, finalListViewLines);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this.tabOwnerActivity, android.R.layout.simple_list_item_1, finalListViewLines);
         usersListView.setAdapter(adapter);
     }
 }
